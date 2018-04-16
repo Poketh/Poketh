3b8b42cd3a0f6ad6eb12a991a0f43b9c62b02bf3
Oscar Fonseca <OFRBG@users.noreply.github.com>
2018-04-16 02:16:59 -0500

    Update MatrixDescriptor.sol
        /*
        [52, 51,  0, 51,  0,
         53,  0, 55, 52,  0,
         54, 53, 53, 55,  0,
         54,  0, 54,  0,  0]
                            
        [ 0, 53, 52,  0,  0,
         53, 52, 52, 57,  0,
          0, 54, 52, 58,  0,
          0, 56, 52, 52,  0]
    
        [55, 55,  0,  0,  0,
         60, 59, 55, 55,  0,
         52, 56, 59,  0,  0,
          0, 54, 60,  0,  0]
          
        [60,  0, 60,  0,  0,
         56, 55, 56,  0,  0,
          0, 60,  0,  0,  0,
          0,  0,  0,  0,  0]
          
        [55, 56,  0,  0,  0,
          0, 60,  0,  0,  0,
          0,  0,  0,  0,  0,
          0,  0,  0,  0,  0]
         */

--- Contracts/MatrixDescriptor.sol
+++ Contracts/MatrixDescriptor.sol
@@ -24,39 +24,29 @@ contract MatrixDescriptor is Ownable {
   uint16 constant matrixSize = 20;
   mapping(uint64 => uint64[matrixSize]) internal items;
   uint64 internal idx;
-
+  
   Pallete internal colors;
 
   function MatrixDescriptor(address _pallete) public {
     colors = Pallete(_pallete);
-    items[1] = [uint64(52), 51,  0, 51,  0,
-                        53,  0, 55, 52,  0,
-                        54, 53, 53, 55,  0,
-                        54,  0, 54,  0,  0];
-                        
-    items[2] = [uint64( 0), 53, 52,  0,  0,
-                        53, 52, 52, 57,  0,
-                         0, 54, 52, 58,  0,
-                         0, 56, 52, 52,  0];
-
-    items[3] = [uint64(55), 55,  0,  0,  0,
-                        53, 59, 55, 55,  0,
-                        52, 56, 59,  0,  0,
-                         0, 54, 53,  0,  0];
-
-    idx = 3;
+    
+    idx = 0;
   }
 
   function addItem(uint64[matrixSize] _itm) public onlyOwner {
     idx = idx + 1;
     items[idx] = _itm;
   }
+  
+  function editItem(uint64 _idx, uint64[matrixSize] _itm) public onlyOwner {
+    items[_idx] = _itm;
+  }
 
   function getItem(uint64 _idx) view public returns(uint64[matrixSize]) {
     uint64[matrixSize] memory itemsHex = items[_idx];
-
-    for (uint i = 0; i < matrixSize; i++) {
-      itemsHex[i] = colors.getColor(itemsHex[i]);
+    
+    for(uint i = 0; i < matrixSize; i++){
+        itemsHex[i] = colors.getColor(itemsHex[i]);
     }
     return itemsHex;
 
