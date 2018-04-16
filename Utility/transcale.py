37537e1da52823431a3b7f36781c6df24c43ac02
Oscar Fonseca <OFRBG@users.noreply.github.com>
2018-04-15 20:19:36 -0500

    Create transcale.py

---
+++ Utility/transcale.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import numpy as np
+
+sizex = 5
+sizey = 4
+
+value = [16227374, 14554653, 0, 14554653, 0, 16707022, 0, 15032114, 16227374, 0, 15582605, 16707022, 16707022, 15032114, 0, 15582605, 0, 15582605, 0, 0]
+vhex = ['{0:06X}'.format(v) for v in value]
+
+npa = np.array(vhex)
+npar = npa.reshape(sizex,sizey)
+
+rgb = np.array([
+                [ [int(v[i:i+2],base=16) for i in (0,2,4)] + [0 if v == '000000' else 255] for v in line ]
+               for line in npar], dtype=np.uint8)
+
+img = Image.fromarray(rgb, 'RGBA')
