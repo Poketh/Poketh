95ed0a4cef1838d587af82950871931c90472514
Oscar Fonseca <OFRBG@users.noreply.github.com>
2018-04-15 20:10:51 -0500

    Update MatrixDescriptor.sol

--- Contracts/MatrixDescriptor.sol
+++ Contracts/MatrixDescriptor.sol
@@ -21,28 +21,33 @@ contract Ownable {
 }
 
 contract MatrixDescriptor is Ownable {
-  mapping(uint64 => uint64[16]) items;
+  uint16 constant matrixSize = 20;
+  mapping(uint64 => uint64[matrixSize]) internal items;
   uint64 internal idx;
-  
-  Pallete colors;
+
+  Pallete internal colors;
 
   function MatrixDescriptor(address _pallete) public {
     colors = Pallete(_pallete);
-    items[1] = [uint64(1), 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    items[1] = [uint64(52), 51, 0, 51, 0,
+      53, 0, 55, 52, 0,
+      54, 53, 53, 55, 0,
+      54, 0, 54, 0, 0
+    ];
 
     idx = 1;
   }
 
-  function addItem(uint64[16] _itm) public onlyOwner {
+  function addItem(uint64[matrixSize] _itm) public onlyOwner {
     idx = idx + 1;
     items[idx] = _itm;
   }
 
-  function getItem(uint64 _idx) view public returns(uint64[16]) {
-    uint64[16] memory itemsHex = items[_idx];
-    
-    for(uint i = 0; i < 16; i++){
-        itemsHex[i] = colors.getColor(itemsHex[i]);
+  function getItem(uint64 _idx) view public returns(uint64[matrixSize]) {
+    uint64[matrixSize] memory itemsHex = items[_idx];
+
+    for (uint i = 0; i < matrixSize; i++) {
+      itemsHex[i] = colors.getColor(itemsHex[i]);
     }
     return itemsHex;
 
