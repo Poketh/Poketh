82be68ca6a2103a9fea9c9b2588344b859f38343
Oscar Fonseca <OFRBG@users.noreply.github.com>
2018-04-15 22:20:08 -0500

    Update transcale.py

--- Utility/transcale.py
+++ Utility/transcale.py
@@ -8,10 +8,11 @@ value = [16227374, 14554653, 0, 14554653, 0, 16707022, 0, 15032114, 16227374, 0,
 vhex = ['{0:06X}'.format(v) for v in value]
 
 npa = np.array(vhex)
-npar = npa.reshape(sizex,sizey)
+npar = npa.reshape(sizey,sizex)
 
 rgb = np.array([
                 [ [int(v[i:i+2],base=16) for i in (0,2,4)] + [0 if v == '000000' else 255] for v in line ]
                for line in npar], dtype=np.uint8)
 
 img = Image.fromarray(rgb, 'RGBA')
+img.resize([200,200])
